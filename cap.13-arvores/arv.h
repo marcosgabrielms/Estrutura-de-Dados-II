7786d67a61a9236474c2eb92da5690c4b76f646e
Marcos Gabriel <marcosgabriel166@gmail.com>
2026-03-04 10:22:35 -0300

    refactor: adapta bilbioteca de árvore para C++

--- cap.13-arvores/arv.h
+++ cap.13-arvores/arv.h
@@ -1,59 +1,54 @@
-#ifndef ARV_H
-#define ARV_H
+#ifndef ARV_HPP
+#define ARV_HPP
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
-#define fmt "%d " 
 typedef int Item; 
 
-
-typedef struct arv {
-    struct arv *esq; 
+struct arv {
+    arv *esq; 
     Item item;       
-    struct arv *dir; 
-} *Arv;              
+    arv *dir; 
+};
+typedef arv* Arv;              
 
 
-Arv arv(Arv e, Item x, Arv d) { 
-    Arv n = (Arv)malloc(sizeof(struct arv)); 
+Arv cria_arv(Arv e, Item x, Arv d) { 
+    Arv n = new arv; 
     n->esq = e;  
     n->item = x; 
     n->dir = d;  
     return n;    
 }
 
-
 void emordem(Arv A) { 
-    if (A == NULL) return; 
+    if (A == nullptr) return; 
     emordem(A->esq); 
-    printf(fmt, A->item); 
+    std::cout << A->item << " "; 
     emordem(A->dir); 
 }
 
-
 void preordem(Arv A) { 
-    if (A == NULL) return; 
-    printf(fmt, A->item); 
+    if (A == nullptr) return; 
+    std::cout << A->item << " "; 
     preordem(A->esq); 
     preordem(A->dir); 
 }
 
-
 void posordem(Arv A) { 
-    if (A == NULL) return; 
+    if (A == nullptr) return; 
     posordem(A->esq); 
     posordem(A->dir); 
-    printf(fmt, A->item);
+    std::cout << A->item << " ";
 }
 
 
-void destroi(Arv *A) { 
-    if (*A == NULL) return; 
-    destroi(&(*A)->esq); 
-    destroi(&(*A)->dir); 
-    free(*A); 
-    *A = NULL; 
+void destroi(Arv& A) { 
+    if (A == nullptr) return; 
+    destroi(A->esq); 
+    destroi(A->dir); 
+    delete A; 
+    A = nullptr; 
 }
 
 #endif
